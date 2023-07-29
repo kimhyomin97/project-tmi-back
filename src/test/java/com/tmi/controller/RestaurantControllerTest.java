945796de5d1bb235cfac2bcd1268777fac9b2d5e
kimhyomin97 <rlagyalsdlek@naver.com>
2023-07-29 23:56:28 +0900

    feat: 프로젝트 환경설정, 테스트코드 연습

---
+++ src/test/java/com/tmi/controller/RestaurantControllerTest.java
@@ -0,0 +1,83 @@
+package com.tmi.controller;//import com.tmi.controller.RestaurantController;
+import com.tmi.controller.RestaurantController;
+import com.tmi.dto.Restaurant;
+import com.tmi.service.RestaurantService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class RestaurantControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private RestaurantService restaurantService;
+
+    @InjectMocks
+    private RestaurantController restaurantController;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(restaurantController).build();
+    }
+
+    @Test
+    public void testGetAllRestaurants() throws Exception {
+        // Given
+        Restaurant restaurant1 = new Restaurant("2023-07-29T12:34:56", "Restaurant 1", "Address 1", "2023-07-29T10:00:00",
+                "Korean", 37.12345, 127.56789);
+        Restaurant restaurant2 = new Restaurant("2023-07-29T12:34:57", "Restaurant 2", "Address 2", "2023-07-29T09:30:00",
+                "Italian", 37.23456, 127.67890);
+
+        List<Restaurant> allRestaurants = Arrays.asList(restaurant1, restaurant2);
+
+        when(restaurantService.findAll()).thenReturn(allRestaurants);
+
+        // When and Then
+        mockMvc.perform(get("/api/restaurants/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name").value("Restaurant 1"))
+                .andExpect(jsonPath("$[0].address").value("Address 1"))
+                .andExpect(jsonPath("$[1].name").value("Restaurant 2"))
+                .andExpect(jsonPath("$[1].address").value("Address 2"));
+    }
+
+    @Test
+    public void testGetNearestRestaurants() throws Exception {
+        // Given
+        Restaurant restaurant1 = new Restaurant("2023-07-29T12:34:56", "Restaurant 1", "Address 1", "2023-07-29T10:00:00",
+                "Korean", 37.12345, 127.56789);
+        Restaurant restaurant2 = new Restaurant("2023-07-29T12:34:57", "Restaurant 2", "Address 2", "2023-07-29T09:30:00",
+                "Italian", 37.23456, 127.67890);
+
+        List<Restaurant> nearestRestaurants = Arrays.asList(restaurant1, restaurant2);
+
+        when(restaurantService.getNearestRestaurants(anyDouble(), anyDouble())).thenReturn(nearestRestaurants);
+
+        // When and Then
+        mockMvc.perform(get("/api/restaurants/near")
+                        .param("lat", "37.12345")
+                        .param("lon", "127.56789"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name").value("Restaurant 1"))
+                .andExpect(jsonPath("$[0].address").value("Address 1"))
+                .andExpect(jsonPath("$[1].name").value("Restaurant 2"))
+                .andExpect(jsonPath("$[1].address").value("Address 2"));
+    }
+}
